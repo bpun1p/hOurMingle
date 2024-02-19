6041de278fa87c29587d56b315f06087ef42c7b2
bpun1p <barry_pun@hotmail.com>
2024-02-19 12:41:10 -0800

    Added fonts on the rootlayout

--- client/src/app/_layout.tsx
+++ client/src/app/_layout.tsx
@@ -1,9 +1,31 @@
 import { Stack } from "expo-router";
 import { useFonts, Pacifico_400Regular  } from '@expo-google-fonts/pacifico'
-import { Inter_400Regular } from "@expo-google-fonts/inter";
+import { Inter_700Bold, Inter_600SemiBold, Inter_400Regular, Inter_900Black } from "@expo-google-fonts/inter";
 import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [ fontsLoaded, fontError ] = useFonts({
+    Pacifico: Pacifico_400Regular,
+    InterBlack: Inter_900Black,
+    InterBold: Inter_700Bold,
+    InterSemi: Inter_600SemiBold,
+    Inter: Inter_400Regular
+  })
+
+  useEffect(() => {
+    //if fonts are loaded or font error; remove splash screen and use default font
+    if(fontsLoaded || fontError) {
+      SplashScreen.hideAsync()
+    }  
+  }, [fontsLoaded, fontError]);
+
+  if(!fontsLoaded && !fontError) {
+    return null;
+  };
+
   return (
     <Stack>
       <Stack.Screen name='index' options={{title: 'hOurMingle'}} />
